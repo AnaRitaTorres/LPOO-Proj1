54e9be6e40c3e4a9552119c123a2e8365470bc7b
Rita <anaritatorres96@outlook.pt>
2016-03-04 10:25:34 +0000

    welelel

--- ProjGuiado.java/src/maze/cli/main.java
+++ ProjGuiado.java/src/maze/cli/main.java
@@ -3,14 +3,8 @@ import maze.logic.Playing;
 
 public class main 
 {
-
 	public static void main(String[] args) 
 	{
-<<<<<<< HEAD
-=======
-		// TODO Auto-generated method stub
-		//o pires teve aqui
->>>>>>> origin/master
 		Playing.update();
 	}
 }
