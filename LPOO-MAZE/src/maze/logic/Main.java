3a25c81e028300f8daa71ed187d5af0553b4d4de
dalugoga <dalugoga@gmail.com>
2016-03-29 17:12:23 +0100

    ficha 2 feita

--- LPOO-MAZE/src/maze/logic/Main.java
+++ LPOO-MAZE/src/maze/logic/Main.java
@@ -4,15 +4,15 @@ import maze.logic.Play;
 public class Main
 {
 	static Play p = new Play();
-	private static MazeBuilder mb = new MazeBuilder(9);
+	//private static MazeBuilder mb = new MazeBuilder(3511);
 	
 	public static void main(String[] args) 
 	{	
 		
-		mb.printMaze();
-		System.out.print('\n');
-		mb.printVisited();
-		//p.gamePlay();
+		//printMaze();
+		//System.out.print('\n');
+		//mb.printVisited();
+		p.gamePlay();
 
 	}
 
