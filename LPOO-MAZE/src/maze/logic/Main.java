34caa31f903545a0c62785b9a62e29f1165bb6b7
Rita <anaritatorres96@outlook.pt>
2016-03-29 17:50:53 +0100

    sem erros

--- LPOO-MAZE/src/maze/logic/Main.java
+++ LPOO-MAZE/src/maze/logic/Main.java
@@ -4,24 +4,24 @@ import maze.logic.Play;
 public class Main
 {
 	static Play p = new Play();
-<<<<<<< HEAD
+
 	//private static MazeBuilder mb = new MazeBuilder(9);
-=======
+
 	//private static MazeBuilder mb = new MazeBuilder(3511);
->>>>>>> origin/master
+
 	
 	public static void main(String[] args) 
 	{	
 		
-<<<<<<< HEAD
+
 //		mb.printMaze();
 //		System.out.print('\n');
 //		mb.printVisited();
-=======
+
 		//printMaze();
 		//System.out.print('\n');
 		//mb.printVisited();
->>>>>>> origin/master
+
 		p.gamePlay();
 
 	}
